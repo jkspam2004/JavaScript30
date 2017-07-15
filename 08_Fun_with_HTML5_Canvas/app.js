43ec144d5afe50a753359fda99d6aa945985c8bd
jkspam2004 <jkspam2004@yahoo.com>
2017-07-14 18:10:37 -1000

    add touch event to canvas

--- 08_Fun_with_HTML5_Canvas/app.js
+++ 08_Fun_with_HTML5_Canvas/app.js
@@ -14,30 +14,38 @@
     //ctx.globalCompositeOperation = "multiply"; // blends to black
 
     let isDrawing = false;
-    let lastX = 0;
-    let lastY = 0;
+    let lastPos = { x: 0, y: 0 };
     let hue = 0;
     let direction = true;
 
+    /* draw the line */
     function draw(e) {
         if (!isDrawing) return;
         console.log(e);
         ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
 
         ctx.beginPath();
-        ctx.moveTo(lastX, lastY);           // start from here
+        ctx.moveTo(lastPos.x, lastPos.y);   // start from here
         ctx.lineTo(e.offsetX, e.offsetY);   // go to here
         ctx.stroke();                       // draw! 
 
-        [lastX, lastY] = [e.offsetX, e.offsetY];
+        [lastPos.x, lastPos.y] = [e.offsetX, e.offsetY]; // update last position
 
-        // change the hue
+        changeHue();
+        changeLineWidth();
+        ctx.closePath();
+    }
+
+    /* change the hue */
+    function changeHue() {
         hue++;
         if (hue >= 360) {
             hue = 0;
         }
+    }
 
-        // change the line width
+    /* change the line width */
+    function changeLineWidth() {
         if (ctx.lineWidth >= 50 || ctx.lineWidth <= 1) {
             direction = !direction;
         } 
@@ -46,23 +54,67 @@
         } else {
             ctx.lineWidth--;
         }
-        ctx.closePath();
     }
 
+    /* clear the canvas */
     function clearCanvas() {
         console.log("clear");
         ctx.clearRect(0, 0, canvas.width, canvas.height);
     }
 
-    // start drawing from current position with mousedown event
+    /* start drawing from current position with mousedown event */
     canvas.addEventListener("mousedown", (e) => {
         isDrawing = true;
-        [lastX, lastY] = [e.offsetX, e.offsetY];
+        [lastPos.x, lastPos.y] = [e.offsetX, e.offsetY];
     });
 
     canvas.addEventListener("mousemove", draw);
     canvas.addEventListener("mouseup", () => isDrawing = false);
     canvas.addEventListener("mouseout", () => isDrawing = false);
+
+    // Set up touch events for mobile, etc
+    canvas.addEventListener("touchstart", (e) => {
+        var touch = e.touches[0];
+        var mouseEvent = new MouseEvent("mousedown", {
+            clientX: touch.clientX,
+            clientY: touch.clientY
+        });
+        canvas.dispatchEvent(mouseEvent);
+    }, false);
+
+    canvas.addEventListener("touchend", (e) => {
+        var mouseEvent = new MouseEvent("mouseup", {});
+        canvas.dispatchEvent(mouseEvent);
+    }, false);
+
+    canvas.addEventListener("touchmove", (e) => {
+        var touch = e.touches[0];
+        var mouseEvent = new MouseEvent("mousemove", {
+            clientX: touch.clientX,
+            clientY: touch.clientY
+        });
+        canvas.dispatchEvent(mouseEvent);
+    }, false);
+
+
+    // Prevent scrolling when touching the canvas
+    document.body.addEventListener("touchstart", (e) => {
+        if (e.target == canvas) {
+            e.preventDefault();
+        }
+    }, false);
+
+    document.body.addEventListener("touchend", (e) => {
+        if (e.target == canvas) {
+            e.preventDefault();
+        }
+    }, false);
+
+    document.body.addEventListener("touchmove", (e) => {
+        if (e.target == canvas) {
+            e.preventDefault();
+        }
+    }, false);
 
 }
 
